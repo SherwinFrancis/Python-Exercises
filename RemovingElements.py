f93daac0cea3cdb1852ab5d464c10203ca6f5aa9
SherwinFrancis <sherwinfrancis71@gmail.com>
2026-01-22 11:09:11 +0000

    Adding function to remove target element in python

---
+++ RemovingElements.py
@@ -0,0 +1,11 @@
+def removeElement(nums, val) :
+    if len(nums) == 0:
+        return 0
+    else:
+        j = 0
+        for i in range(0, len(nums)):
+            if not nums[i] == val:
+                nums[j] = nums[i]
+                j += 1
+        return j
+
